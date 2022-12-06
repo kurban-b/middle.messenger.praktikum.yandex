81b0a25e336c4519a0432fdfaeb8666681365178
kurban_b <bdobik@mail.ru>
2022-12-06 05:29:21 +0300

    доработал сервер для раздачи статики

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use('/', express.static(`${__dirname}/dist`));
+app.use(express.static(path.resolve(__dirname, "./dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./dist/index.html"));
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
